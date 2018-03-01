1c3d22243681b6a9170f9506e9c021e22ba87e6f
RBK28 <rbk28@RBK28s-Mac-mini.local>
2018-03-01 14:47:25 +0200

    our final project

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
+//console.log("hey")
 
 
-console.log("hey")
 
 jQuery(document).ready(function ($) {
 
@@ -46,5 +46,67 @@ jQuery(document).ready(function ($) {
     $('a.control_next').click(function () {
         moveRight();
     });
+//======================= input text 
+    
+    
+    $("#addbutton").click(function(){
+      var com = $('#input').val()
+      console.log(com)
+    $("#review").append("<p style='background-color: #90EE90; text-align: center;margin: auto; width: 60%; border: 3px solid #90EE90; border-radius: 12px; padding: 10px;'>" + com + "</p>")
+    
+    $("#input").val("")
+
+    })
+
+//======================== function for lists 
+
+//var butt = document.getElementById("one")
+
+// var array = [{con:" <p><a href='#'>Link</a></p><ul><li>Tortilla Meatball Soup</li><li>Saffron Fish Stew with White Beans</li><li>Summer Squash and Corn Chowder</li><li>Broccoli-Cheese Soup</li></ul>", id:"one"},
+//                   {con:"2", id:"two"},
+//                   {con:"3", id:"three"},
+//                   {con:"4", id:"four"},
+//                   {con:"5", id:"five"}];
+
+ 
+
+// var arr=[$("#one"),$("#two"),$("#three"),$("#four"),$("#five")]
+
+
+// for (var i = 0 ; i < arr.length; i++) {
+  
+//       arr[i].click(function(){
+//     //console.log()
+//    for (var j = 0; j < array.length ; i++) {
+//     if ( arr[i].attr("id")=== array[i].id) {
+//       //console.log(array[i].con)
+//       $("#list").html(array[i].con)
+//     }
+
+//     console.log(x.attr("id"))
+//   }
+
+  
+
+// });
+
+
+
+// }
+
+  //========= lists 
+
+    //for soups con 1
+  
+    // for healthy con 2 
+  
+    // for Appetizers  con 3
+
+    // for vegan con 4
+
+    // for salads con 5
+     
+
+
 
 }); 
